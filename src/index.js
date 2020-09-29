626147006c88d60969bcd16834eb5b7c68334923
7k7k <2365012335@qq.com>
2020-09-29 15:33:10 +0800

    .gitignore update

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'antd/dist/antd.css';
+
+// import PCIndex from './components/pc_index'
+
+class Index extends React.Component{
+    render(){
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+
+ReactDOM.render(
+    <Index></Index>,
+    document.getElementById("root")
+)
